64de4b9a4782223e7d20bec5673125295c6c0a24
Rizvan Dzhamaludinov <Rizvan06101996@gmail.com>
2021-11-26 13:57:11 +0300

    настройка сборки

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+        }
+      }
+    ]
+  }
+}
